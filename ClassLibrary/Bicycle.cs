ee49ca99fc0e9cfd3a4d7f621c10423b0da70472
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2022-02-11 16:26:22 +0200

    Implemented class Bicycle.

--- ClassLibrary/Bicycle.cs
+++ ClassLibrary/Bicycle.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    public class Bicycle
+    public class Bicycle : Vehicle
     {
+        protected string Color { set; get; }
+        protected double WheelDiameter { set; get; }
+
+        public Bicycle(string color, double wheelDiameter, double kilometrage)
+        {
+            Color = color;
+            WheelDiameter = wheelDiameter;
+            Odometer = kilometrage;
+            VisitedLocalities = new List<string>();
+        }
+
+        public override string GetInfo()
+        {
+            return "Bicycle (" + Color + ", " + WheelDiameter.ToString() + " cm wheel): " + Odometer.ToString() + "km";
+        }
     }
 }
